f78ef369671c2531ae92e02d6e5b13ed842b7245
Erikas Kunca <kunca.erikas@gmail.com>
2019-02-25 21:04:18 +0200

    Added camera boundaries and following of player character.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject PlayerCharacter;
+    //Bounds are to be set by the maker of the area.
+    public GameObject LeftBound;
+    public GameObject TopBound;
+    public GameObject RightBound;
+    public GameObject BottomBound;
+
+    private Vector3 Offset;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Offset = transform.position - PlayerCharacter.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        var position = PlayerCharacter.transform.position + Offset;
+        var Camera = GetComponent<Camera>();
+        position = new Vector3
+        (
+            Mathf.Clamp(position.x, LeftBound.transform.position.x + Camera.orthographicSize * Camera.aspect, RightBound.transform.position.x - Camera.orthographicSize * Camera.aspect),
+            Mathf.Clamp(position.y, BottomBound.transform.position.y + Camera.orthographicSize, TopBound.transform.position.y - Camera.orthographicSize),
+            position.z);
+        transform.position = position;
+    }
+}
