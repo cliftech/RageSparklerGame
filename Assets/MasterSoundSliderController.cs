2517211e1f5f918a7f4a0dd6a4e6be528caa5d04
Erikas Kunca <kunca.erikas@gmail.com>
2019-04-10 14:30:37 +0300

    ARL10 & MAM2

---
+++ Assets/MasterSoundSliderController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterSoundSliderController : MonoBehaviour
+{
+    public UnityEngine.UI.Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.value = PlayerPrefs.HasKey("MasterVolume")?PlayerPrefs.GetFloat("MasterVolume"):1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
