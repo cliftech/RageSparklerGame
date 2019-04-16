afe17f1dc1fd6724e746e71bf9b0719f2d965346
Erikas Kunca <kunca.erikas@gmail.com>
2019-04-16 16:47:38 +0300

    BUG16

---
+++ Assets/Scripts/ReturnToMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ReturnToMenu : MonoBehaviour
+{
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
